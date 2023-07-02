40d7ceb3f1377406ac8c7352f58f18d19241db03
Kirill <apatski94@gmail.com>
2023-07-02 21:14:15 +0300

    feat: add styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/700.css'
+
 import { StrictMode } from 'react'
 
 import { createRoot } from 'react-dom/client'
 
 import { App } from './App.tsx'
-import './index.css'
+import './styles/index.scss'
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
